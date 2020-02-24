5c85aea86e2d2ecd3d9e8beb6f2561505ac28654
rickbala <ricardovbalarini@gmail.com>
2020-02-24 11:51:49 -0300

    posting and showing

--- src/main/java/com/rickbala/stream2me/controller/StreamController.java
+++ src/main/java/com/rickbala/stream2me/controller/StreamController.java
@@ -1,22 +1,37 @@
 package com.rickbala.stream2me.controller;
 
+import com.rickbala.stream2me.utils.Utils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import com.rickbala.stream2me.utils.Utils;
 
 @Controller
 public class StreamController {
 
-	private static String text = "";
+	private static String text = "sou um texto do backend";
 
-	@GetMapping("/getStream")
+	@GetMapping("/decypher")
 	@ResponseBody
-	public String getStream(){;
+	public String decypher(){;
 		text = text + Utils.getRandomLetter();
 		if (text.length() % 80 == 0) text = text + "<br/>";
 		System.out.println(text);
 		return text;
+	}
+
+	@GetMapping("/getStream")
+	@ResponseBody
+	public String getStream(){
+		return text;
+	}
+
+	@GetMapping("/postStream")
+	@ResponseBody
+	public void postStream(@RequestParam String var){
+		System.out.println("chegou algo para postar=" + var);
+		text = text + var;
+
 	}
 
 }
